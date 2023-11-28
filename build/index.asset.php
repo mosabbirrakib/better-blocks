fb01fd9889c1df80bd6c03db791fb5d2ec6df000
Md. Al-Mosabbir Rakib <mrakib50.cse@gmail.com>
2023-11-28 14:26:34 +0600

    Create table block.

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'f95275f06a79302dc08e');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'ab222529ef4c5a1fc76a');
